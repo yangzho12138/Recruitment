e570bd35057afcfb8e01cbba6d4ff501f95f7d0e
yz96 <68858113+yangzho12138@users.noreply.github.com>
2022-12-30 23:01:31 -0600

    joblist

--- jobs/views.py
+++ jobs/views.py
@@ -1,6 +1,33 @@
-from django.shortcuts import render
+from django.http import JsonResponse
 from jobs.models import Job
+from jobs.models import Cities, JobTypes
+
 
 # Create your views here.
+
+# get all the job
 def joblist(request):
-    job_list = Job.objects.order_by('job-type')
+    job_list = Job.objects.order_by('job_type')
+
+    data = []
+    if job_list:
+        for job in job_list:
+            j = {}
+            j['job_type'] = JobTypes[job.job_type][1]
+            j['job_city'] = Cities[job.job_city][1]
+            j['job_name'] = job.job_name
+            j['job_responsibility'] = job.job_responsibility
+            j['job_requirement'] = job.job_requirement
+            # foreign key type: <class 'django.contrib.auth.models.User'> , can not be serializable, transfer to str
+            j['creator'] = str(job.creator)
+            j['created_date'] = job.created_date
+            j['modified_date'] = job.modified_date
+            data.append(j)
+
+    return JsonResponse({
+        'status': 200,
+        'message': 'success',
+        'data': data
+    })
+
+
--- recruitment/urls.py
+++ recruitment/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import include
+from django.urls import re_path as url
+
 
 urlpatterns = [
+    url(r"^", include("jobs.urls")),
     path("admin/", admin.site.urls),
 ]
