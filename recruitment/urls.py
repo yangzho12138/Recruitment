4373267f5f8d76fba5a0f04018c56147e8b8639c
yz96 <68858113+yangzho12138@users.noreply.github.com>
2023-01-05 11:27:01 -0600

    django theme

--- recruitment/urls.py
+++ recruitment/urls.py
@@ -22,6 +22,7 @@ from django.utils.translation import gettext as _
 
 urlpatterns = [
     url(r"^", include("jobs.urls")),
+    path('grappelli/', include('grappelli.urls')),
     path("admin/", admin.site.urls),
 ]
 
